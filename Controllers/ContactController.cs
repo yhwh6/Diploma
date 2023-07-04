5f2b63ce4d57004c82fb7e41c7580e75abc3bd07
Maksym_Sylantiev <maxym2s@yahoo.com>
2023-07-04 16:41:25 +0300

    Updated Contacts Views and fixed _Layout

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 using Diploma.Models;
 
 namespace Diploma.Controllers
@@ -14,9 +15,78 @@ namespace Diploma.Controllers
 
         public IActionResult Index()
         {
-            // Получение контактной информации из базы данных
             var contacts = _context.Users.FirstOrDefault();
             return View(contacts);
+        }
+
+        [Authorize(Roles = "Administrator")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Administrator")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Contact contact)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        [Authorize(Roles = "Administrator")]
+        public IActionResult Edit(int id)
+        {
+            var contact = _context.Contacts.Find(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return View(contact);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Administrator")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Contact contact)
+        {
+            if (id != contact.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(contact);
+        }
+
+        [Authorize(Roles = "Administrator")]
+        public IActionResult Delete(int id)
+        {
+            var contact = _context.Contacts.Find(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return View(contact);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Administrator")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var contact = _context.Contacts.Find(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ Views/Contacts/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Diploma.Views.Contacts
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
